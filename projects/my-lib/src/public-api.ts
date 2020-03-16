c0d4a4a76b793d67fb1d552dbef24ec6ec2126c0
Igor Pak <imme1987@rambler.ru>
2020-03-16 23:08:41 +1000

    library rebased ++

--- projects/my-lib/src/public-api.ts
+++ projects/my-lib/src/public-api.ts
@@ -4,5 +4,6 @@
 
 export * from './lib/my-lib.module';
 export * from './lib/hello.component/hello.component';
+export * from './lib/bye-component/bye-component';
 
 export * from './lib/base.service';
